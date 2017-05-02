450fc8cf906e8a572bb55d4829b3e3c5d64c69df
alex <alexbonavila44@gmail.com>
2017-05-02 19:59:25 +0200

    Card montada falta cache

--- resources/views/cards/_card.blade.php
+++ resources/views/cards/_card.blade.php
@@ -1,11 +1,69 @@
 {{--@cache($card) --}}
-    <article class="Card">
-        <h2>{{ $card->title }}</h2>
 
-        <ul>
-            @foreach ($card->notes as $note)
-                @include ('cards/_note')
-            @endforeach
-        </ul>
-    </article>
+{{--<div class="card">--}}
+  {{--<div class="card-image">--}}
+    {{--<figure class="image is-4by3">--}}
+      {{--<img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">--}}
+    {{--</figure>--}}
+  {{--</div>--}}
+  {{--<div class="card-content">--}}
+    {{--<div class="media">--}}
+      {{--<div class="media-left">--}}
+        {{--<figure class="image is-48x48">--}}
+          {{--<img src="http://bulma.io/images/placeholders/96x96.png" alt="Image">--}}
+        {{--</figure>--}}
+      {{--</div>--}}
+      {{--<div class="media-content">--}}
+        {{--<p class="title is-4">John Smith</p>--}}
+        {{--<p class="subtitle is-6">@johnsmith</p>--}}
+      {{--</div>--}}
+    {{--</div>--}}
+
+    {{--<div class="content">--}}
+      {{--Lorem ipsum dolor sit amet, consectetur adipiscing elit.--}}
+      {{--Phasellus nec iaculis mauris. <a>@bulmaio</a>.--}}
+      {{--<a>#css</a> <a>#responsive</a>--}}
+      {{--<br>--}}
+      {{--<small>11:09 PM - 1 Jan 2016</small>--}}
+    {{--</div>--}}
+  {{--</div>--}}
+{{--</div>--}}
+
+    <article class="Card">
+
+        <div class="card">
+  <div class="card-image">
+    {{--<figure class="image is-4by3">--}}
+      {{--<img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">--}}
+    {{--</figure>--}}
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        {{--<figure class="image is-48x48">--}}
+          {{--<img src="http://bulma.io/images/placeholders/96x96.png" alt="Image">--}}
+        {{--</figure>--}}
+      </div>
+      <div class="media-content">
+        <p class="title is-4">{{ $card->title }}</p>
+      </div>
+    </div>
+
+    <div class="content">
+        @foreach ($card->notes as $note)
+            @include ('cards/_note')
+        @endforeach
+    </div>
+  </div>
+</div>
+
+
+        {{--<h2>{{ $card->title }}</h2>--}}
+
+        {{--<ul>--}}
+            {{--@foreach ($card->notes as $note)--}}
+                {{--@include ('cards/_note')--}}
+            {{--@endforeach--}}
+        {{--</ul>--}}
+    {{--</article>--}}
 {{--@endcache--}}
